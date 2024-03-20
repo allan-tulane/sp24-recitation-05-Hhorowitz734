4d278c483759cd3e532a3669a867d77d8f65d090
Benjamin_Horowitz <hhorowitz734@gmail.com>
2024-03-20 14:57:35 -0500

    finished lab

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Reciation 5
 ## Answers
 
-**Name:**_________________________
+**Name:**Benjamin Horowitz
 
 
 Place all written answers from `recitation-05.md` here for easier grading.
@@ -12,9 +12,14 @@ Place all written answers from `recitation-05.md` here for easier grading.
 
 
 
-- **1b.**
+- **1b.**  
+**How do the running times compare to the asymptotic bounds? **
+
+Quicksort works in nlogn time, which is consistent with the results we got. Timsort also works is nlogn time, but according to the python documentation it is optimized for real world data, which may explain its good performance for the unsorted array case. 
 
 
 
 
-- **1c.**
+- **1c.** 
+**How does changing the type of input list change the relative performance of these algorithms? **
+Quicksort with the fixed low pivot performs very poorly for sorted arrays, as it bins every number into 1 bucket (as they will all be greater than the pivot), which reduces the time complexity to O(n^2).
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import random, time
 import tabulate
 
+#THIS PARTITION FUNCTION IS ADAPTED ENTIRELY FROM THE LECTURE
+def partition(a, low, high, pivot_fn):
+    pivot_index = pivot_fn(a, low, high)
+    pivot = a[pivot_index]
+    a[pivot_index], a[high] = a[high], a[pivot_index]
+    i = low
+    for j in range(low, high):
+        if a[j] < pivot:
+            a[i], a[j] = a[j], a[i]
+            i += 1
+    a[i], a[high] = a[high], a[i]
+    return i
+#LECTURE CODE ENDS HERE
+
 def ssort(L):
     ### selection sort
     if (len(L) == 1):
@@ -12,9 +26,34 @@ def ssort(L):
         print('recursively sorting L=%s\n' % L[1:])
         return [L[0]] + selection_sort(L[1:])
         
-def qsort(a, pivot_fn):
-    ## TO DO
-    pass
+def qsort(a, pivot_fn, low = 0, high = None):
+
+    if high is None:
+        #We leave high to be an optional parameter, setting it as the idx of the last element
+        high = len(a) - 1
+    
+    if low < high: #We must make sure this is the case before we perform any operation
+        
+        #Partition our array a accordingly
+        part = partition(
+            a, low, high, pivot_fn
+        )
+        
+        #Now, we can send both parts to qsort
+        qsort(a, pivot_fn, low, part - 1)
+        qsort(a, pivot_fn, part + 1, high)
+        #This is reminiscent of binary search
+    
+    return a
+
+#This function is the case that we use the first element as the pivot
+def first_pivot(a, low, high):
+    return low #low is idx of the first element
+
+#Case 2 -> We have a random pivot
+def rand_pivot(a, low, high):
+    return random.randint(low, high)
+
     
 def time_search(sort_fn, mylist):
     """
@@ -50,32 +89,59 @@ def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 10
       for each method to run on each value of n
     """
     ### TODO - sorting algorithms for comparison
-    qsort_fixed_pivot = # 
-    qsort_random_pivot = #
-    tim_sort = #
+    qsort_fixed_pivot = lambda l: qsort(l, first_pivot)
+    qsort_random_pivot = lambda l: qsort(l, rand_pivot) #Using lambda function, we test qsort with different pivots
+    tim_sort = sorted #Since this is built in into python
     result = []
     for size in sizes:
         # create list in ascending order
         mylist = list(range(size))
         # shuffles list if needed
-        #random.shuffle(mylist)
+        random.shuffle(mylist)
+
+        #Implemented the tests for all our sorting algos
         result.append([
-            len(mylist),
-            time_search(qsort_fixed_pivot, mylist),
-            time_search(qsort_random_pivot, mylist),
+            size,
+            time_search(qsort_fixed_pivot, mylist.copy()),
+            time_search(qsort_random_pivot, mylist.copy()),
+            time_search(tim_sort, mylist.copy())
         ])
     return result
     ###
 
 def print_results(results):
     """ change as needed for comparisons """
+    
     print(tabulate.tabulate(results,
-                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot'],
-                            floatfmt=".3f",
-                            tablefmt="github"))
+        headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot', 'tim_sort'],
+        floatfmt=".3f",
+        tablefmt="github"))
+
 
 def test_print():
     print_results(compare_sort())
 
 random.seed()
 test_print()
+
+'''
+Using random permutations
+------------------------
+|      n |   qsort-fixed-pivot |   qsort-random-pivot |   tim_sort |
+|--------|---------------------|----------------------|------------|
+|    100 |               0.111 |                0.140 |      0.009 |
+|    200 |               0.214 |                0.283 |      0.017 |
+|    500 |               0.633 |                0.855 |      0.046 |
+|   1000 |               1.395 |                2.017 |      0.100 |
+|   2000 |               3.092 |                3.870 |      0.233 |
+|   5000 |               8.394 |               10.649 |      0.605 |
+|  10000 |              18.453 |               22.178 |      1.280 |
+|  20000 |              38.971 |               48.260 |      2.905 |
+|  50000 |             107.383 |              128.790 |      7.811 |
+| 100000 |             220.212 |              285.027 |     16.898 |
+'''
+
+
+'''
+Using pre-sorted lists
+'''
